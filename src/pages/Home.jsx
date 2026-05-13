fc315aa34d17b2f9849dc155f917c25a3a2c20ac
Upendra Gupta <ug6805757@gmail.com>
2026-05-13 14:23:24 +0530

    hjh

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@
 import Hero from '@/components/Hero'
 import  Feature from '@/components/Feature'
 import React from 'react'
-
+import Productcard from '@/components/Productcard'
+  
 const Home = () => {
   return (
     <div>
      <Hero/>
      <Feature />
+     <Productcard/>
     </div>
   )
 }
